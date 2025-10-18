9a4ea572627e16233e00479250278adf2c131bf4
tamakou <tamakou1217@gmail.com>
2025-10-19 05:01:05 +0900

    Remove RealityKitContent dependency

--- StarSaber/ContentView.swift
+++ StarSaber/ContentView.swift
@@ -7,7 +7,6 @@
 
 import SwiftUI
 import RealityKit
-import RealityKitContent
 
 struct ContentView: View {
 
@@ -19,8 +18,21 @@ struct ContentView: View {
 
         ZStack {
             RealityView { content in
-                if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
-                    content.add(scene)
+                if content.entities.isEmpty {
+                    let root = Entity()
+
+                    let padMesh = MeshResource.generatePlane(width: 1.4, depth: 1.4)
+                    let padMaterial = SimpleMaterial(color: .init(red: 0.08, green: 0.08, blue: 0.12, alpha: 1), roughness: 0.85, isMetallic: false)
+                    let padEntity = ModelEntity(mesh: padMesh, materials: [padMaterial])
+                    padEntity.position = [0, -0.02, 0]
+                    root.addChild(padEntity)
+
+                    let orbMaterial = SimpleMaterial(color: .init(red: 0.1, green: 0.5, blue: 1.0, alpha: 1), roughness: 0.1, isMetallic: true)
+                    let orb = ModelEntity(mesh: .generateSphere(radius: 0.18), materials: [orbMaterial])
+                    orb.position = [0, 0.35, 0]
+                    root.addChild(orb)
+
+                    content.add(root)
                 }
             }
             .ignoresSafeArea()
