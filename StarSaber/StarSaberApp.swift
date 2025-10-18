6b1b7c19b64a7fba796f2e6fa5b747473ba29348
tamakou <tamakou1217@gmail.com>
2025-10-19 00:59:46 +0900

    Initial Commit

---
+++ StarSaber/StarSaberApp.swift
@@ -0,0 +1,34 @@
+//
+//  StarSaberApp.swift
+//  StarSaber
+//
+//  Created by tamakou on 2025/10/19.
+//
+
+import SwiftUI
+
+@main
+struct StarSaberApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
---
+++ StarSaberTests/StarSaberTests.swift
@@ -0,0 +1,17 @@
+//
+//  StarSaberTests.swift
+//  StarSaberTests
+//
+//  Created by tamakou on 2025/10/19.
+//
+
+import Testing
+@testable import StarSaber
+
+struct StarSaberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
